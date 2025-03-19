767724f2d64cc4ffed04070ee6c477f3152c79c1
desarrollador27-collab <desarrollador27@tecsersas.com>
2025-03-19 17:02:41 -0500

    Second commit
    Dates and time: 19-03-2025 - 17:02

---
+++ src/main/java/com/projectest/sigcon/models/Containeryard.java
@@ -0,0 +1,126 @@
+package com.projectest.sigcon.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+public class Containeryard {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String equipment;//this is the number container 
+	
+	
+	// this methods will utilize for modify or update number container.
+	public void setEquipment(String equipment) {this.equipment = equipment;}
+	
+	
+	public String getEquipment() {
+		return equipment;
+	}
+	
+	
+	public enum Size_equipment{
+		TWENTY_FOOT,
+		FOURTY_FOOT;
+		
+	}
+	
+	public enum Type_equipment{
+		DRY_VAN,
+		HIGH_CUBE,
+		HIGH_CUBE_REFORCE,
+		REEFER,
+		OPEN_TOP,
+		FLAT_RACK;
+		
+	}
+	
+	public enum State_equipment{
+		DAMAGED,
+		DIRTY,
+		GOOD;
+		
+	}
+	
+
+	public static class Containerdefine{ //Define a Container with its properties
+		
+		
+		private Containeryard containeryard;
+		private Size_equipment size;
+		private Type_equipment type;
+		private State_equipment state;
+		
+
+		public Size_equipment getSize() {
+			return size;
+		}
+
+
+
+
+		public void setSize(Size_equipment size) {
+			this.size = size;
+		}
+
+
+
+
+		public Type_equipment getType() {
+			return type;
+		}
+
+
+
+
+		public void setType(Type_equipment type) {
+			this.type = type;
+		}
+
+
+
+
+		public State_equipment getState() {
+			return state;
+		}
+
+
+
+
+		public void setState(State_equipment state) {
+			this.state = state;
+		}
+
+	
+		@Override
+		public String toString() {
+		    return "Container{" +
+                    "Equipment='" + containeryard.getEquipment() + '\'' +
+                    ", Size=" + getSize()+
+                    ", type=" + getType()+
+                    ", Status='" + getState()+ '\'' +
+                    '}';
+		}
+		
+		
+	}
+	
+		
+}
+	
+
+	
+	
+	
+	
+	
+
+
---
+++ src/main/java/com/projectest/sigcon/models/Users.java
@@ -0,0 +1,60 @@
+package com.projectest.sigcon.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+	
+	public enum Role_User{
+		ADMIN,
+		USER;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String Fullname;
+	private String Email;
+	private String Phone;
+	private String Password;
+	private Role_User role_user;
+	
+	public String getFullname() {
+		return Fullname;
+	}
+	public void setFullname(String fullname) {
+		Fullname = fullname;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public Role_User getRole_user() {
+		return role_user;
+	}
+	public void setRole_user(Role_User role_user) {
+		this.role_user = role_user;
+	}
+
+	
+	
+
+}
