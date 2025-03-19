fad27d93207732d39c09e68811e131770f251f3e
desarrollador27-collab <desarrollador27@tecsersas.com>
2025-03-19 12:12:25 -0500

    First commit
    First Date: 19-03-2025

---
+++ src/main/java/com/projectest/sigcon/SigconApplication.java
@@ -0,0 +1,13 @@
+package com.projectest.sigcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigconApplication.class, args);
+	}
+
+}
