e90a3613b9759ea391e2f589539f38cf85b2e4cf
desarrollador27-collab <desarrollador27@tecsersas.com>
2025-03-21 17:30:39 -0500

    se realiza DTO y servicios de usuarios
    date: 21-03-2025 time: 17:30

--- src/main/java/com/projectest/sigcon/dto/UserDTO.java
+++ src/main/java/com/projectest/sigcon/dto/UserDTO.java
@@ -1,71 +1,86 @@
-package com.projectest.sigcon.models;
+package com.projectest.sigcon.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import com.projectest.sigcon.models.Users.Role_User;
 
-@Entity
-@AllArgsConstructor
-public class Users {
-	
-	public enum Role_User{
-		ADMIN,
-		USER;
+public class UserDTO {
 
-	}
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
-	
-
 	private String fullname;
 	private String email;
 	private String phone;
 	private String password;
 	private Role_User role_user;
 	
+	public UserDTO() {}
+	
+	public UserDTO(Long id, String fullname, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.fullname = fullname;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+	
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getFullname() {
 		return fullname;
 	}
+
 	public void setFullname(String fullname) {
 		this.fullname = fullname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Role_User getRole_user() {
 		return role_user;
 	}
+
 	public void setRole_user(Role_User role_user) {
 		this.role_user = role_user;
 	}
 
-	
-	
 
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", Fullname='" + fullname + '\'' +
+                ", email='" + email + '\'' +
+                ", Phone='" + phone + '\'' +
+                '}';
+    }
+	
+	
+	
 }
--- src/main/java/com/projectest/sigcon/services/UserService.java
+++ src/main/java/com/projectest/sigcon/services/UserService.java
@@ -1,14 +1,66 @@
 package com.projectest.sigcon.services;
 
 
+import java.util.List;
+
+import com.projectest.sigcon.dto.UserDTO;
+import com.projectest.sigcon.models.Users;
 import com.projectest.sigcon.repository.UserRepository;
 
 public class UserService {
 	
 	
 	private final UserRepository userRepository = null;
+	
+	
+	// Services to create User and save in DataBase
+	public UserDTO createUser(UserDTO userdto) {
+		
+		if(userRepository.existsById(userdto.getId())) {
+			throw new IllegalArgumentException("Usuario no disponible, ya existe");
+		}
+		Users user = new Users();
+		user.setFullname(userdto.getFullname());
+		user.setEmail(userdto.getEmail());
+		user.setPhone(userdto.getPhone());
+		user.setPassword(userdto.getPassword());
+		user.setRole_user(userdto.getRole_user());
+		
+		Users saveuser = userRepository.save(user);
+		return mapUserToDTO(saveuser);
+	
+	}
+	
+	// Services to find All Users created in the DataBase
+	public List<UserDTO> findAllUsers(){
+		
+		return userRepository.findAll().stream().map(this::mapUserToDTO).toList();
+	}
+	
+	
+	public Users updateUser(Long id, UserDTO userdto) {
+		
+		return null;
+	}
 
 
+	
+	
+	
+	
+	
+	
+	private UserDTO mapUserToDTO(Users user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFullname(user.getFullname());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPhone(user.getPhone());
+        userDTO.setRole_user(user.getRole_user());
+
+        return userDTO;
+    }
+	
 	
 
 }
